5d7cb0d8d95a366804aae8e3d73ccf820adfb339
Chris Toews <ctoews@mit.edu>
2015-11-06 11:40:49 -0500

    try messaging again

--- PeopleSync/src/edu/mit/ist/salesforce/sync/Main.java
+++ PeopleSync/src/edu/mit/ist/salesforce/sync/Main.java
@@ -1,14 +1,78 @@
 package edu.mit.ist.salesforce.sync;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 
 public class Main {
 
-	public static void main(String[] args){
+	public static void notmain(String[] args){
 		System.out.println("You are inside Main");
 	}
 	
+	public static void main(String[] args) {
+		 String username ;
+		 String password;
 
+		
+		Properties getProp = new Properties();
+		InputStream input = null;
+		
+		try{
+			input = new FileInputStream("config.properties");
+
+			// load a properties file
+			getProp.load(input);
+			username = getProp.getProperty("username");
+			password = getProp.getProperty("password");
+		}catch(Exception ex){
+			ex.printStackTrace();
+			username = System.getenv("DATABASE_URL");
+			password = System.getenv("password");
+		}
+		
+		final String finalUser = username;
+		final String finalPass = password;
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(finalUser, finalPass);
+			}
+		  });
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("herokumailforme@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("ctoews@mit.edu"));
+			message.setSubject("FunMail");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}//end of main
 	
 }//end of class
