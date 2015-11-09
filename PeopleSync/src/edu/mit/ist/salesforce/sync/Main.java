e9d00aecedc226485ff3d34a6b93a7554227c92b
Chris Toews <ctoews@mit.edu>
2015-11-09 10:27:07 -0500

    added an email to each run

--- PeopleSync/src/edu/mit/ist/salesforce/sync/Main.java
+++ PeopleSync/src/edu/mit/ist/salesforce/sync/Main.java
@@ -22,6 +22,7 @@ public class Main {
 
 	
 	public static void main(String[] args) throws URISyntaxException, SQLException{
+		emailMe();
 		Connection conn = getConnection();
 		PreparedStatement readPS = conn.prepareStatement("select * from sftest1.account");
 		ResultSet readRS = readPS.executeQuery();
